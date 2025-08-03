4d99072a0da2ca31ea9d3b89bc4b90fed59d5c92
sandeeppatel98 <patelspdp98@gmail.com>
2025-08-03 23:03:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const quotes = [
   "Push yourself, because no one else is going to do it for you.",
   "Great things never come from comfort zones.",
   "Success doesn’t just find you. You have to go out and get it.",
-  "Don’t watch the clock; do what it does. Keep going. — Sam Levenson"
+  "Don’t watch the clock; do what it does. Keep going. — Sam Levenson",
+  "Start where you are. Use what you have. Do what you can. — Arthur Ashe",
+  "Dream big and dare to fail. — Norman Vaughan"
 ];
 
 function showQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  document.getElementById("newQuote").innerText = quotes[randomIndex];
+  document.getElementById("currentQuote").innerText = quotes[randomIndex];
 }
